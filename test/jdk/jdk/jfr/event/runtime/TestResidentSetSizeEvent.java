ec03ea4104f30e0057124029d279acde2d335802
MBaesken <matthias.baesken@sap.com>
2026-07-15 15:30:35 +0200

    JDK-8388316

--- test/jdk/jdk/jfr/event/runtime/TestResidentSetSizeEvent.java
+++ test/jdk/jdk/jfr/event/runtime/TestResidentSetSizeEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -36,6 +36,7 @@ import jdk.test.lib.jfr.Events;
 
 /**
  * @test
+ * @key intermittent
  * @requires vm.flagless
  * @requires vm.hasJFR
  * @library /test/lib
